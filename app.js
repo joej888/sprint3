38010c38c5da20756b8168883f6a015a1e2c7f0b
joej888 <123joemonjacob@gmail.com>
2021-05-07 13:20:55 +0530

    Updating for Generate OTP

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const { Sentry } = require('vod-npm-sentry');
 const { restifyErrorMiddleware } = require('vod-npm-utils/error');
 
 const LOGGER = createLogger({
-  name: 'vod-ms-app-checkout-authentication',
+  name: 'vod-ms-upgrade-journey-authentication',
   level: config.get('log.level')
 });
 
